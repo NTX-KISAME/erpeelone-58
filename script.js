92ac3dfa7161673a7cc20a6824d8541dc023b892
adiiaryasuta <adiaryasuta.dev@gmail.com>
2021-06-30 13:26:42 +0800

    responsive navbar

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ window.addEventListener('scroll', function() {
     let navbar = document.querySelector('nav');
     navbar.classList.toggle('navbar-scrolled', window.scrollY > 0);
 });
+
+const menu = document.querySelector('header nav ul');
+const toggle = document.querySelector('header nav .menu-toggle input');
+
+toggle.addEventListener('click', function() {
+    menu.classList.toggle('slide');
+});
